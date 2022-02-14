ef37e254659098eebc798d67e5da537ff671fb5b
Wojciech Kapsa <wojciech.kapsa@medvc.eu>
2022-02-14 12:41:01 +0100

    Custom gst build (1.20.0) in Dockerfile.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,32 @@ set -e
 set -o pipefail
 
 apt-get update && \
-DEBIAN_FRONTEND=noninteractive apt-get -y install sudo git build-essential apt-utils x264 cmake libcpprest-dev libfmt-dev libnss3-dev libcups2-dev libxdamage-dev libxcomposite-dev libatk1.0-dev libatk-bridge2.0-dev libxkbcommon-dev libxrandr-dev libgbm-dev libasound2-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly meson ninja-build xvfb
+DEBIAN_FRONTEND=noninteractive apt-get -y install sudo git build-essential apt-utils x264 cmake libcpprest-dev libfmt-dev libnss3-dev libcups2-dev libxdamage-dev libxcomposite-dev libatk1.0-dev libatk-bridge2.0-dev libxkbcommon-dev libxrandr-dev libgbm-dev libasound2-dev xvfb python3-pip bison flex
+
+pip3 install meson ninja
+
+cd /tmp &&
+git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git && \
+cd  gstreamer && \
+git checkout 1.20.0 && \
+meson --buildtype release \
+    -Dgtk_doc=disabled \
+    -Ddoc=disabled \
+    -Dtests=disabled \
+    -Dexamples=disabled \
+    -Dpython=disabled \
+    -Dlibav=enabled \
+    -Dugly=enabled \
+    -Dbad=enabled \
+    -Ddevtools=disabled \
+    -Drs=disabled \
+    -Dgpl=enabled \
+    -Dges=disabled \
+    -Drtsp_server=disabled \
+    -Dvaapi=disabled \
+    --strip build && \
+ninja -C build install && \
+ldconfig
  
 cd /tmp &&
 git clone https://github.com/centricular/gstcefsrc.git && \
